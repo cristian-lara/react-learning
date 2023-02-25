14b95a5d19a860292ea7a45727f2e549073f8643
Cristian Lara <cristian89lara@gmail.com>
2023-02-25 17:05:45 -0600

    feat: renderizado gifs

--- 03-App-React-con-API/buscador-gif/src/components/gift-grid.component.tsx
+++ 03-App-React-con-API/buscador-gif/src/components/gift-grid.component.tsx
@@ -1,21 +1,40 @@
 import {getGifts} from "../helpers/get-gifs";
 import {useEffect, useState} from "react";
-
+export interface Igif {
+    id: string,
+    title: string,
+    url: string
+}
 export const GiftGridComponent = ({categoria}: any)=> {
-    const [counter, setCounter] = useState(10);
     // use efect ejecuta efectos secundarios, disparar cualquier cosa que necesitamos
+    const [gifs, setGifs] = useState([]);
+    const getGifs = async ()=> {
+        const newGifs = await  getGifts(categoria);
+        setGifs(newGifs);
+    }
     useEffect(() => {
-        getGifts(categoria);
+         getGifs();
+
     }, []); // si esta vacio significa que solo se dispara una vez cuando se crea el componente
 
     return(
         <>
             <div>
-                <h3>
+                <h2>
                     {categoria.id} - {categoria.nombre}
-                </h3>
-                <h5> {counter}</h5>
-                <button onClick={()=> setCounter(counter +1)}>+1</button>
+                </h2>
+<ol>
+    { gifs.map((gif: Igif) => (
+
+            <li key={gif.id}>
+                {gif.title}
+            </li>
+
+    ) ) }
+</ol>
+
+
+
             </div>
         </>
             )
