26b4c689f24cafdc8784c894ce15e093123b6dd5
Cristian Lara <cristian89lara@gmail.com>
2023-02-25 17:54:54 -0600

    feat: condicional dentro de mi FC

--- 03-App-React-con-API/buscador-gif/src/components/gift-grid.component.tsx
+++ 03-App-React-con-API/buscador-gif/src/components/gift-grid.component.tsx
@@ -19,6 +19,16 @@ export const GiftGridComponent = ({categoria}: any)=> {
                 <h2>
                     {categoria.id} - {categoria.nombre}
                 </h2>
+                {/* forma uno de usar
+                    {
+                        isLoading
+                        ? (<h2>...Cargando</h2>)
+                        : null
+                }
+                */}
+                {
+                    isLoading && (<h2>...Cargando</h2>)
+                }
                 <div className="card-grid">
                      { images.map((image: Igif) => (
 
