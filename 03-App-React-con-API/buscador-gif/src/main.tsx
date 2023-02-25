0660c4af48dfd3c028199bdbb903dba055e84b66
Cristian Lara <cristian89lara@gmail.com>
2023-02-25 16:46:02 -0600

    feat: explicacion  del stric mode en react
    leer docu oficial para saber que no mas hace

--- 03-App-React-con-API/buscador-gif/src/main.tsx
+++ 03-App-React-con-API/buscador-gif/src/main.tsx
@@ -4,6 +4,7 @@ import GliphyApp from "./gliphyapp";
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    // ayuda a validar muchas cosa por y va arrojar warnings si encuentra algun error pero en prod no entra en accion
   <React.StrictMode>
     <GliphyApp />
   </React.StrictMode>,
