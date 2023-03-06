89366b4133451dd6767e773e8ab5516e5fb78f55
Cristian Lara <cristian89lara@gmail.com>
2023-03-06 15:11:27 -0500

    feat: use effect en formulario

---
+++ 04-hooks/hook-app/src/02-UseEffect/SimpleForm.jsx
@@ -0,0 +1,52 @@
+import {useEffect, useState} from "react";
+
+export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        userName: '',
+        email: 'a@a.com'
+    });
+
+    const {userName, email} = formState
+
+    const onInputChange = ({target}) => {
+        const {name, value} = target
+        setFormState({...formState, [name]: value})
+    }
+
+    // efectos aqui
+// primer argumento el callback
+    // arreglo de depencias es decir razones por als cuales quiero disparar si esta vacio indica que solo queremos se ejecute una vez
+    // podemos tener varios use efect para cada accion que queremos controlar
+    useEffect(() => {
+      console.log('cambio el formulario')
+    }, [formState]);
+    useEffect(() => {
+        console.log('cambio el email')
+    }, [email]);
+    return (
+        <>
+            <h1>Simple Form</h1>
+            <hr/>
+            <div className="row">
+                <div className="col-md-6">
+                    <input type="text"
+                           className="form-control mt-2"
+                           placeholder="User Name"
+                           name="userName"
+                           value={userName}
+                           onChange={onInputChange}
+                    />
+                </div>
+                <div className="col-md-6">
+                    <input type="text"
+                           className="form-control mt-2"
+                           placeholder="example@a.com"
+                           name="email"
+                           value={email}
+                           onChange={onInputChange}
+                    />
+                </div>
+            </div>
+        </>
+    );
+};
--- 04-hooks/hook-app/src/HooksApp.jsx
+++ 04-hooks/hook-app/src/HooksApp.jsx
@@ -1,12 +1,13 @@
 import Button from "bootstrap/js/src/button.js";
 import {CounterApp} from "./01-use-state/CounterApp.jsx";
 import {CounterWithCustomHook} from "./01-use-state/CounterWithCustomHook.jsx";
+import {SimpleForm} from "./02-UseEffect/SimpleForm.jsx";
 
 export const HooksApp = () => {
     return (
         <>
-            <CounterWithCustomHook/>
-
+            {/*<CounterWithCustomHook/>*/}
+            <SimpleForm/>
         </>
     );
 };
