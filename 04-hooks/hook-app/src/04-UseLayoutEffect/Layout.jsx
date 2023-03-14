e380f64f3662706055f2c176403ed3aa97c91cc5
Cristian Lara <cristian89lara@gmail.com>
2023-03-14 11:32:33 -0500

    feat: uselayout effect

---
+++ 04-hooks/hook-app/src/04-UseLayoutEffect/Layout.jsx
@@ -0,0 +1,43 @@
+import {UseCounter} from "../hooks/UseCounter.js";
+import {UseFetch} from "../hooks/UseFetch.js";
+import {Loading, QuoteBB} from "../components/index.js";
+
+export const Layout =  () => {
+
+
+    // console.log('data:' ,data, 'isLoading', isLoading,'hasError', hasError)
+    const {increment, decrement, counter} = UseCounter(1)
+    console.log('counter', counter)
+    const urlMain = `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`
+    const {data, isLoading, hasError}= UseFetch(urlMain);
+
+    const onChangeUrl = ()=>{
+        increment(1);
+    }
+    return (
+        <>
+            <h1>Multiple custom hook</h1>
+            <hr/>
+            {
+                isLoading ? (
+                        <Loading/>
+                    )
+                    :
+                    (
+                        data.map((value, index)=>{
+                            return (
+                                <QuoteBB key={index} {...value}/>
+                            )
+                        })
+
+
+                    )
+            }
+
+            <button className="btn btn-info mx-2" onClick={decrement}>Previus</button>
+            <button className="btn btn-info" onClick={onChangeUrl}>Next</button>
+
+
+        </>
+    );
+};
--- 04-hooks/hook-app/src/main.jsx
+++ 04-hooks/hook-app/src/main.jsx
@@ -4,9 +4,10 @@ import './index.css'
 import {HooksApp} from "./HooksApp.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {FocusScreen} from "./03-use-ref/FocusScreen.jsx";
+import {Layout} from "./04-UseLayoutEffect/Layout.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FocusScreen />
+    <Layout />
   </React.StrictMode>,
 )
