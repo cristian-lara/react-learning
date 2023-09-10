3fb1b75bdf6ea8534599b6ad5a448bac2f07c557
Cristian Lara <cristian89lara@gmail.com>
2023-09-09 23:41:22 -0700

    added dark conditions in a button

--- 05-tailwind/react-tailwind/src/App.tsx
+++ 05-tailwind/react-tailwind/src/App.tsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [theme, setTheme] = useState("light");
+
+  useEffect(()=> {
+    if(theme === 'dark'){
+      document.querySelector('html')?.classList.add('dark');
+    }else{
+      document.querySelector('html')?.classList.remove('dark');
+    }
+  }, [theme])
+
+  const handleThemeColor = () => {
+    setTheme(theme=> theme === "light" ? "dark" : "light")
+  }
+
   return (
-      <div className="h-screen flex justify-center items-center text-5xl"
-      >APP</div>
+      <div className="h-screen flex justify-center items-center text-5xl bg-slate-500 dark:bg-black"
+      >
+        <button onClick={handleThemeColor} 
+        className='bg-slate-200 text-black px-4 py-2 rounded hover:bg-slate-300  dark:bg-slate-600 dark:text-white dark:hover:bg-slate-900'> Change theme</button>
+      </div>
   )
 }
 
